db12a3cc0f0c46ecc4d173ca5c22233df785beaf
arpao007 <pao123zpao123z@gmail.com>
2025-07-24 12:40:45 +0700

    add Lab3

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -2,43 +2,41 @@
 #include <math.h>
 
 int isPrime(int num) {
-    if (num < 2) return 0;
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) return 0;
-    }
+
+  if(num < 2)return 0;
+    for (int i = 2; i <= sqrt (num); i++) {
+
+  if (num % i == 0)return 0;}
     return 1;
-}
+  }
 
 int main() {
-    int n;
-    printf("Enter N : ");
-    scanf("%d", &n);
+  int n;
+  printf("Enter N : ");scanf("%d",&n);
 
-    if (n <= 0) {
-        printf("Invalid input. N must be greater than 0.\n");
-        return 1;
-    }
+  int arr[n]; for(int i = 0; i <n ; i++){
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        printf("Enter value[%d] : ", i);
-        scanf("%d", &arr[i]);
-    }
+  printf("Enter value[%d] : ",i);
+  scanf("%d" ,& arr[i]);
+  }
 
-    printf("Index:");
-    for (int i = 0; i < n; i++) {
-        printf("%3d", i);
-    }
-    printf("\n");
+  printf("Index :  ");
+  for (int i = 0; i < n; i++){
 
-    printf("Array:");
-    for (int i = 0; i < n; i++) {
-        if (isPrime(arr[i]))
-            printf("%3d", arr[i]);
-        else
-            printf("%3s", "#");
-    }
-    printf("\n");
+  printf("%2d ", i);
+  }  
 
-    return 0;
-}
+  printf("\n");
+  printf("Array:  ");
+    for(int i = 0; i < n; i++){if (isPrime(arr[i]))
+  printf("%2d ", arr[i]); 
+
+  else
+  printf("%2s ", "#"); 
+  }
+
+  printf("\n"); 
+  
+  return 0;
+  
+  }

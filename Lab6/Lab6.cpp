0f0ba020f5607bbf16d731100de205c0d32c043c
arpao007 <pao123zpao123z@gmail.com>
2025-08-09 22:35:08 +0700

    add Lab6

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int N;
+    printf( " Enter value:" );
+    scanf("%d",&N);
+     if (N % 2 == 1) {
+        printf("Series: 1%d\n", N);
+        for 
+     }
+
+}
